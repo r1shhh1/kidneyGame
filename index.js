d3c405971508a5d058e3ea45d75317b640ec6802
r1shhh1 <rishi29ganguly@gmail.com>
2024-01-22 19:26:27 +0530

    ini

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+var users = [
+  {
+    name: "john",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
+
+//get request get input in queryparameters
+app.get("/", function (req, res) {
+  const johnKidneys = users[0].kidneys;
+  const numberOfKidneys = johnKidneys.length;
+  let numberOfHealthyKidneys = 0;
+  for (let i = 0; i < johnKidneys.length; i++) {
+    if (johnKidneys[i].healthy) {
+      numberOfHealthyKidneys += 1;
+    }
+  }
+
+  const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+  res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys,
+    numberOfUnhealthyKidneys,
+  });
+});
+
+app.post("/", function (req, res) {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "DONE!",
+  });
+});
+
+app.put("/", function (req, res) {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json();
+});
+
+//remove all unhealthy kidneys
+app.delete("/", function (req, res) {
+  const newKidneys = [];
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (users[0].kidneys[i].healthy) {
+      newKidneys.push({
+        healthy: true,
+      });
+    }
+  }
+  users[0].kidneys = newKidneys;
+  res.json({ msg: "Done!" });
+});
+
+app.listen(3000, () => {
+  console.log("ready");
+});
